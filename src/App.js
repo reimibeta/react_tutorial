a1a26b2dcb145fe5e22b5d7e815532821e7f90a6
reimi <65539218+reimibeta@users.noreply.github.com>
2021-12-17 13:20:34 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Nav } from 'react-bootstrap';
 import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [name, setName] = useState('');
+
+    useEffect(() => {
+        (
+            async () => {
+                // const response = await fetch('http://localhost:8000/api/user', {
+                //     headers: {'Content-Type': 'application/json'},
+                //     credentials: 'include',
+                // });
+
+                // const content = await response.json();
+
+                // setName(content.name);
+            }
+        )();
+    });
+
+
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Nav name={name} setName={setName}/>
+
+                <main className="form-signin">
+                    <Routes>
+                      <Route path="/" element={<Home />} />
+                      <Route path="/login" element={<Login />} />
+                      {/* <Route path="/login" component={() => <Login setName={setName}/>}/>
+                      <Route path="/register" component={Register}/> */}
+                    </Routes>
+                </main>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
