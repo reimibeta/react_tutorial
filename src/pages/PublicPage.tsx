83b7ea2825c87d0b6c51ad99233a7d1b8a74060c
reimi <65539218+reimibeta@users.noreply.github.com>
2021-12-19 16:25:50 +0700

    Update PublicPage.tsx

--- src/pages/PublicPage.tsx
+++ src/pages/PublicPage.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
-interface UserInterface {
+interface MyProps {
     readonly id: number,
     name: string,
-    password?: string
+    password?: string,
+    who: string,
+    object: { a: string, b: string }
 }
 
 // const user: UserInterface = {
 //     id: 1,
 //     name: 'reimi'
 // }
-// const PublicPage: React.FC<UserInterface> = ({name}: UserInterface) => {
-const PublicPage = (props: { who: string }) => {
+// const PublicPage: React.FC<MyProps> = ({name}: MyProps) => {
+// const PublicPage = (props: { who: string }) => {
+const PublicPage = (props: MyProps) => {
     return <p>Hello, { props.who } from Public Page.</p>; 
 }
 export default PublicPage;
